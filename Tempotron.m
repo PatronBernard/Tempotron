4bfcc25e865ed7f9196f432e5e5469616da01d16
Jan <jan.morez@gmail.com>
2016-01-11 18:28:37 +0100

    sdsdfsdc

--- Tempotron.m
+++ Tempotron.m
@@ -1,66 +1,52 @@
-function V=Tempotron(n_a, t)
-    %n_a    Number of afferents
-    %t      Vector with discretized time
-    I_input=zeros(size(t));
-    w=rand(10);
-    for i=1:n_a
+function [V]=Tempotron(n_afferents, t)
+    %n_a    Number of afferents, e.g. 5 or 10
+    %t      Vector with discretized time, e.g. t=linspace(0,0.5,1000);  
+    
+    w=rand(10); %For now we just generate random weights
+    V_thresh=1; %Threshold voltage
+    
+    tau=0.015;  %Membrane integration time constant
+    
+    V_sub=zeros(size(t)); %Submembrane voltage
+    
+    %Generate a random pattern and calculate the output with random
+    %weights.
+    for i=1:n_afferents
         Kn(i,:)=reshape(K(t,rand(3)/2),1,length(t));
-        I_input=I_input+w(i)*Kn(i,:);
+        V_sub=V_sub+w(i)*Kn(i,:);
     end
     
-    close all; imagesc(Kn); colormap('hot');
-    xlabel('t (ms)'); ylabel('n'); colorbar;
+    %See if the threshold voltage was crossed
+    [~,idx]=find(V_sub > V_thresh);
+    V=V_sub;
     
-    figure; plot(t,I_input);
-    %hold on; plot(t,I_input); hold off;
-    
-end
-
-function V=LIF(t,I_ext)
-    %Input arguments
-    %adapt_flag     1 for an adaptation current, 0 no adaptation current
-    %tau_adapt      Adaptation current time constant
-    %delta_adapt    Adaptation current increment for each spike
-    
-    %Model parameters that are unchanged (therefore hardcoded)
-    E=-1;      %Hyperpolarized clamping potential 
-    Em=0;%Membrane resting potential 
-    Cm=0.01;       %Membrane capacitance
-    Rm=10;      %Membrane resistance
-    theta=1;   %AP threshold
-    t_arp=0.001;  %Absolute refractory Period
-    tau_adapt=0.01;
-    delta_adapt=10;
-    adapt_flag=1;
-    %Simulation parameters
-    dt=t(2)-t(1);    %Timestep
-    %tmax=100;    %Simulation time
-    
-    %Discretize time
-    tn=t;
-    
-    %Initialization of variables
-    u=zeros(size(tn));
-    u(1)=Em;                    %Initial condition
-    g_adapt=zeros(size(tn));    
-    
-    t0=-t_arp;  %Time when a peak last has occurred
-    t=0;    %Time variable that increments with dt amount
-    
-    for j=1:(length(tn)-1)
-        g_adapt(j+1)=g_adapt(j)-dt*g_adapt(j)/tau_adapt;        %Integrate the evolution of the adaptation current
-        if u(j) >= theta                                        %If the treshold is reached, clamp to E for t_arp
-            u(j+1)=E;                               
-            g_adapt(j+1)=g_adapt(j)+delta_adapt;                %Also increase the adaptation current
-            t0=t;                                               %Save the time when the peak occurred
-        elseif t-t0 < t_arp                                     %During a spike we start counting the time
-            u(j+1)=E;                                           %Clamp for t_arp to E   
-        else
-            u(j+1)=u(j)+dt*((Em-u(j))/(Rm*Cm)+I_ext(j)/Cm-adapt_flag*g_adapt(j)*(u(j)-Em)/Cm); 
-        end
-        t=t+dt;        
+    %If so, reduce the voltage smoothly to 0.
+    if ~isempty(idx)
+        idx=min(idx);
+        V(idx:end)=V_sub(idx).*exp(-(t(idx:end)-t(idx))/tau);
     end
-    V=u;
+    if nargout==0
+        close all; 
+        %Plot the individual afferent activity in an image
+        subplot(2,1,1)
+        imagesc(Kn); colormap('hot');
+        set(gca,'YTick',1:n_afferents);
+        xlabel('t (ms)'); ylabel('n');
+        
+        %Plot the subthreshold membrane voltage and the neuron's response
+        subplot(2,1,2)
+        plot(t,V,'k','LineWidth',2);   
+        
+        hold on
+        plot(t,V_sub,'Color',[0.5 0.5 0.5])
+        legend('Output voltage','Subthreshold membrane voltage');
+        
+        plot(t,V_thresh.*ones(size(t)),'k-.');
+        xlabel('t (ms)'); ylabel('V (AU)');
+        hold off;
+        
+        set(gcf,'Position',[125 189 1115 789])
+    end
 end
 
 function V=K(t,t_spikes)
@@ -70,7 +56,7 @@ function V=K(t,t_spikes)
     %Model parameters
     tau=0.015;      %Membrane integration time constant
     tau_s=tau/4;    %Synaptic current time constant
-    V0=2.12;        %Normalization
+    V0=2.12;        %Normalization (see paper)
     
     %The step function is required to acquire the correct form of the
     %spikes (i.e. 0 everywhere except after some spike). 
